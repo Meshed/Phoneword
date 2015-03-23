a20233d81524b70207f494f0e9d10583725fb252
Mark Brown <marksbrown@gmail.com>
2015-03-23 10:53:20 -0400

    Initial add

---
+++ Phoneword/CallHistoryActivity.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Phoneword
+{
+	[Activity (Label = "@string/callHistory")]			
+	public class CallHistoryActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Create your application here
+			SetContentView (Resource.Layout.CallHistory);
+
+			var phoneNumbers = Intent.GetStringArrayListExtra ("phone_numbers") ?? new String[0];
+			var adapter = new ArrayAdapter<string> (this, Android.Resource.Layout.SimpleListItem1, phoneNumbers);
+
+			var list = FindViewById<ListView> (Resource.Id.PhoneNumberList);
+			list.Adapter = adapter;
+		}
+	}
+}
+
---
+++ Phoneword/MainActivity.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Core;
+using Android.Views.InputMethods;
+using System.Collections.Generic;
+
+namespace Phoneword
+{
+	[Activity (Label = "Phoneword", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		List<string> _phoneNumbers = new List<string>();
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			var callButton = FindViewById<Button> (Resource.Id.CallButton);
+			var translateButton = FindViewById<Button> (Resource.Id.TranslateButton);
+			var phoneNumberText = FindViewById<EditText> (Resource.Id.PhoneNumberText);
+			var callHistoryButton = FindViewById<Button> (Resource.Id.CallHistoryButton);
+
+			callButton.Enabled = false;
+
+			string translatedNumber = "";
+			translateButton.Click += delegate {
+				var inputMethodManager = (InputMethodManager)GetSystemService(Context.InputMethodService);
+				inputMethodManager.HideSoftInputFromWindow(phoneNumberText.WindowToken, 0);
+
+				translatedNumber = PhonewordTranslator.ToNumber (phoneNumberText.Text);
+
+				if(string.IsNullOrEmpty(translatedNumber))
+				{
+					callButton.Text = "Call";
+					callButton.Enabled = false;
+				}
+				else
+				{
+					callButton.Text = "Call " + translatedNumber;
+					callButton.Enabled = true;
+				}
+
+			};
+
+			callButton.Click += delegate {
+				var callDialog = new AlertDialog.Builder(this);
+
+				callDialog.SetMessage("Call" + translatedNumber + "?");
+
+				callDialog.SetNeutralButton("Call", delegate {
+					_phoneNumbers.Add(translatedNumber);
+					callHistoryButton.Enabled = true;
+					var callIntent = new Intent(Intent.ActionCall);
+					callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
+					StartActivity(callIntent);
+				});
+
+				callDialog.SetNegativeButton("Cancel", delegate {
+					// Nothing to do
+				});
+
+				callDialog.Show();
+			};
+
+			callHistoryButton.Click += delegate {
+				var intent = new Intent(this, typeof(CallHistoryActivity));
+				intent.PutStringArrayListExtra("phone_numbers", _phoneNumbers);
+				StartActivity(intent);
+			};
+		}
+	}
+}
